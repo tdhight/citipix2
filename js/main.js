23b93c6cff016a6c6c81787652c2e8ccec172375
tdhight <tonyahight@Tonyas-MacBook-Pro.local>
2017-03-02 21:05:09 -0500

    Getting image links correct
    Getting image links correct

--- js/main.js
+++ js/main.js
@@ -9,24 +9,21 @@
 
 
 $(document).ready(function(){
-	console.log('this is running');
 
 	function newCity() {
 		var city = $('#city-type').val();
 		if (city === 'New York' || city === 'New York City' || city === 'NYC') {
-			console.log('nyc');
-			$('body').css({'background': 'url(../images/nyc.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/nyc.jpg) no-repeat fixed', 'background-size': 'cover'});
 		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area') {
-			console.log('sf');
-			$('body').css({'background': 'url(../images/sf.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/sf.jpg) no-repeat fixed', 'background-size': 'cover'});
 		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX') {
-			$('body').css({'background': 'url(../images/la.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/la.jpg) no-repeat fixed', 'background-size': 'cover'});
 		} else if (city === 'Austin' || city === 'ATX') {
-			$('body').css({'background': 'url(../images/austin.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/austin.jpg) no-repeat fixed', 'background-size': 'cover'});
 		} else if (city === 'Sydney' || city === 'SYD') {
-			$('body').css({'background': 'url(../images/sydney.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/sydney.jpg) no-repeat fixed', 'background-size': 'cover'});
 		} else {
-			$('body').css({'background': 'url(../images/citipix_skyline.jpg) no-repeat fixed', 'background-size': 'cover'});
+			$('body').css({'background': 'url(images/citipix_skyline.jpg) no-repeat fixed', 'background-size': 'cover'});
 		}
 	}
 	$('#submit-btn').click(function(event) {   
